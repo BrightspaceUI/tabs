90c8e31054a86ff30ae6b48ca581969b76ce6f0b
Dave Batiste <dave.batiste@d2l.com>
2020-04-02 16:00:43 -0400

    Use tabs from core.

--- demo/d2l-demo-templated-tabs.js
+++ demo/d2l-demo-templated-tabs.js
@@ -1,5 +1,4 @@
 import '@polymer/polymer/polymer-legacy.js';
-import 'd2l-button/d2l-button-subtle.js';
 import '../d2l-tabs.js';
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 const $_documentContainer = document.createElement('template');
@@ -10,10 +9,8 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-demo-templated-tabs">
 			<template items="{{tabs}}" is="dom-repeat">
 				<d2l-tab-panel text="{{item.text}}">{{item.content}} <a href="http://www.google.com">Google</a></d2l-tab-panel>
 			</template>
-			<d2l-button-subtle text="Change title" on-click="changeText"></d2l-button-subtle>
 		</d2l-tabs>
 	</template>
-
 </dom-module>`;
 
 document.head.appendChild($_documentContainer.content);
@@ -26,8 +23,5 @@ Polymer({
 			{text: 'Fall 18', content: 'Content for fall 2018'},
 			{text: 'All', content: 'Content for all'}
 		];
-	},
-	changeText: function() {
-		this.set(`tabs.${Math.floor(Math.random() * 4)}.text`, 'Tab Name Changed');
 	}
 });
